dbb3a9b81618c380d5c8468bff7fe2e1a5aab18b
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-10 12:39:51 -0500

    progress

--- plugin/greet.lua
+++ plugin/greet.lua
@@ -1,1 +1,9 @@
-print("greet/plugin/init.lua called")
+vim.api.nvim_create_user_command(
+  "Greet",
+  function(opts)
+    local greet = require("greet").greet
+    local arg1 = opts.args[1]
+    greet(arg1)
+  end,
+  {}
+)
