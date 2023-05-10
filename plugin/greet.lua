4c54bd2e081cae9b52f3cfce05e9f19d3af26f9f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-10 12:45:36 -0500

    debugging

--- plugin/greet.lua
+++ plugin/greet.lua
@@ -3,7 +3,8 @@ vim.api.nvim_create_user_command(
   function(opts)
     vim.print(opts)
     local greet = require("greet").greet
-    local arg1 = opts.args[1]
+    local arg1 = opts.fargs[1]
+    print("arg1 =", arg1)
     greet(arg1)
   end,
   {}
