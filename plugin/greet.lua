6b8bc350089b3fe90dc6669f34efda6e3971ac82
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-10 12:53:07 -0500

    handled missing argument

--- plugin/greet.lua
+++ plugin/greet.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_user_command(
     local greet = require("greet").greet
 
     -- Get the first argument.
-    local arg1 = opts.fargs[1]
+    local arg1 = opts.fargs[1] or ""
 
     -- Remove surrounding quotes.
     local unquoted = arg1:gsub('"(%w*)"', "%1")
